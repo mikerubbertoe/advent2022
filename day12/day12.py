6ce0506a9aeb0ef51a1cf1f4e9ce428dc5048956
Michael Roberto <mikero@vt.edu>
2022-12-14 10:05:27 -0500

    it works I guess

--- day12/day12.py
+++ day12/day12.py
@@ -1,18 +1,17 @@
 import heapq as heap
-import time
 
 class Node:
-    def __init__(self, height, location):
+    def __init__(self, height, location, top):
         self.height = height
         self.cost = 1
         self.location = location
+        self.top = top
 
-    def __str__(self):
-        return self.height
+    def __repr__(self):
+        return "{0}".format(self.location)
 
 
 def dijstra(graph, startingNode):
-
     pq = []
     not_visited = set()
     distance = [float('inf')] * (len(graph) * len(graph[0]))
@@ -27,10 +26,10 @@ def dijstra(graph, startingNode):
         #print(currNode)
         not_visited.remove(currNode[1])
         for n in get_neighbors(currNode[1], len(graph), len(graph[0])):
-            y = int(n / len(graph))
+            y = int(n / len(graph[0]))
             x = int(n % len(graph[0]))
             nextNode = graph[y][x]
-            thisNode = graph[int(currNode[1] / len(graph))][int(currNode[1] % len(graph[0]))]
+            thisNode = graph[int(currNode[1] / len(graph[0]))][int(currNode[1] % len(graph[0]))]
             cost = graph[y][x].cost
 
             if thisNode.height + 1 < nextNode.height:
@@ -38,18 +37,21 @@ def dijstra(graph, startingNode):
 
             if n in not_visited:
                 old_cost = distance[n]
-                new_cost = distance[currNode[1].location] + cost
+                new_cost = distance[thisNode.location] + cost
 
                 if new_cost < old_cost:
                     distance[len(graph[0])*y+x] = new_cost
-                    heap.heappush(pq,(new_cost,graph[y][x]))
+                    heap.heappush(pq,(new_cost,graph[y][x].location))
 
-    print(distance[-1])
-    print(len(distance))
+    for i in graph:
+        for j in i:
+            if j.top:
+                return distance[j.location]
+
 
 def get_neighbors(node, a, b):
     neighbors = []
-    currY = int(node / a)
+    currY = int(node / b)
     currX = int(node % b)
     for x,y in [[0,1],[-1,0],[1,0],[0,-1]]:
         newX = x + currX
@@ -61,17 +63,40 @@ def get_neighbors(node, a, b):
 def parse_input(filename):
     i = []
     counter = 0
+    low = []
     with open(filename) as f:
         for line in f:
             line = line.strip()
             x = []
+
             for char in line:
-                value = ord(char) - 96 if ord(char) - 96 > 0 else 0
-                x.append(Node(value, counter))
+                value = 0
+                top = False
+                if char == 'E':
+                    value = ord('z') - 96
+                    top = True
+                elif char == 'S':
+                    value = 0
+                else:
+                    value = ord(char) - 96
+                x.append(Node(value, counter, top))
+                if char == 'a':
+                    low.append(counter)
                 counter += 1
             i.append(x)
-    return i
+    return i, low
 
-input = parse_input('day12input.txt')
+def part1():
+    input, _ = parse_input('day12input.txt')
+    dijstra(input, 1620)
 
-dijstra(input, 0)
+
+def part2():
+    input, low = parse_input('day12input.txt')
+    lowest = float('inf')
+    for start in low:
+        if (ret := dijstra(input, start)) < lowest:
+            lowest = ret
+    print(lowest)
+
+part2()
